12349d0be5efd80b5deced1f59ba67981528ebc1
jasminesprout <jasminesprout1@gmail.com>
2022-09-14 20:48:33 -0700

    first commit

---
+++ contracts/Raffle.sol
@@ -0,0 +1,82 @@
+// Create contract
+
+// Make people be able to enter the lottery, paying some amount
+
+// Pick a random winner (verifiably random!)
+
+// Winner to be selected every X minutes -> completely automated
+
+// We'll use a Chainlink Oracle to get the randomness from outside the blockchain
+// and Chainlink Keepers to execute the contract automatically
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle is VRFConsumerBaseV2 {
+    // State variables
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_keyHash;
+
+    // Events
+
+    event RaffleEnter(address indexed player);
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_keyHash = gasLane;
+    }
+
+    function enterRaffle() public payable {
+        // require (msg.value > i_entranceFee, "Not enough ETH!")
+
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+        // Emit an event when we update a dynamic array or mapping
+        // Named events with the function name reversed
+        emit RaffleEnter(msg.sender);
+    }
+
+    function pickRandomWinner() external {
+        // Request random number
+        // Once we get it, do something with it
+        // Chainlink VRF is a 2-transaction process
+
+        i_vrfCoordinator.requestRandomWords(
+            keyHash, // gasLane
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    // View / Pure functions
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
